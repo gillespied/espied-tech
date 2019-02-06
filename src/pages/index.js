367c08850a6cd740d515a2e23c48594c57fdc813
David Gillespie <david@espied.tech>
2019-02-06 18:39:23 +0000

    linting and refactor to stateless functions

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,41 +1,37 @@
-import React from "react";
-import PropTypes from "prop-types";
-import Layout from "../components/Layout";
-import { Link } from "gatsby";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'gatsby';
+import Layout from '../components/Layout';
 
-export default class IndexPage extends React.Component {
-  render() {
-    return (
-      <Layout>
-        <section className="hero is-link is-fullheight-with-navbar has-background-primary">
-          <div className="hero-body">
-            <div className="container">
-              <p className="title">
-                Freelance Data Science and Web Development
-              </p>
-              <p className="subtitle">
-                espied(dot)tech is me, David Gillespie I am an experienced data
-                science developer based in Nottingham. Available for both
-                freelance or contract work.
-              </p>
-              <p className="subtitle">
-                To discuss how I could help make sense of your data get in touch
-                either with{" "}
-                <Link style={{ textDecoration: "underline" }} to="./contact/">
-                  the contact form
-                </Link>{" "}
-                or{" "}
-                <Link
-                  style={{ textDecoration: "underline" }}
-                  to="https://www.linkedin.com/in/davidjgillespie/"
-                >
-                  add me on linkedin.
-                </Link>
-              </p>
-            </div>
-          </div>
-        </section>
-      </Layout>
-    );
-  }
-}
+const IndexPage = () => {
+  <Layout>
+    <section className="hero is-link is-fullheight-with-navbar has-background-primary">
+      <div className="hero-body">
+        <div className="container">
+          <p className="title">Freelance Data Science and Web Development</p>
+          <p className="subtitle">
+            espied.tech is the home page for me, David Gillespie. I offer data science development
+            on a freelance or contract basis. I'm based in Nottingham, but also available for remote
+            work.
+          </p>
+          <p className="subtitle">
+            To discuss how I could help make sense of your data get in touch either with
+            {' '}
+            <Link style={{ textDecoration: 'underline' }} to="./contact/">
+              the contact form
+            </Link>
+            {' '}
+            or
+            {' '}
+            <Link
+              style={{ textDecoration: 'underline' }}
+              to="https://www.linkedin.com/in/davidjgillespie/"
+            >
+              add me on linkedin.
+            </Link>
+          </p>
+        </div>
+      </div>
+    </section>
+  </Layout>;
+};
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { kebabCase } from 'lodash'
-import Helmet from 'react-helmet'
-import { graphql, Link } from 'gatsby'
-import Layout from '../components/Layout'
-import Content, { HTMLContent } from '../components/Content'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { kebabCase } from 'lodash';
+import Helmet from 'react-helmet';
+import { graphql, Link } from 'gatsby';
+import Layout from '../components/Layout';
+import Content, { HTMLContent } from '../components/Content';
 
 export const BlogPostTemplate = ({
   content,
@@ -14,7 +14,7 @@ export const BlogPostTemplate = ({
   title,
   helmet,
 }) => {
-  const PostContent = contentComponent || Content
+  const PostContent = contentComponent || Content;
 
   return (
     <section className="section">
@@ -22,17 +22,15 @@ export const BlogPostTemplate = ({
       <div className="container content">
         <div className="columns">
           <div className="column is-10 is-offset-1">
-            <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
-              {title}
-            </h1>
+            <h1 className="title is-size-2 has-text-weight-bold is-bold-light">{title}</h1>
             <p>{description}</p>
             <PostContent content={content} />
             {tags && tags.length ? (
-              <div style={{ marginTop: `4rem` }}>
+              <div style={{ marginTop: '4rem' }}>
                 <h4>Tags</h4>
                 <ul className="taglist">
                   {tags.map(tag => (
-                    <li key={tag + `tag`}>
+                    <li key={`${tag}tag`}>
                       <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
                     </li>
                   ))}
@@ -43,8 +41,8 @@ export const BlogPostTemplate = ({
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
 BlogPostTemplate.propTypes = {
   content: PropTypes.node.isRequired,
@@ -52,10 +50,10 @@ BlogPostTemplate.propTypes = {
   description: PropTypes.string,
   title: PropTypes.string,
   helmet: PropTypes.object,
-}
+};
 
 const BlogPost = ({ data }) => {
-  const { markdownRemark: post } = data
+  const { markdownRemark: post } = data;
 
   return (
     <Layout>
@@ -63,28 +61,26 @@ const BlogPost = ({ data }) => {
         content={post.html}
         contentComponent={HTMLContent}
         description={post.frontmatter.description}
-        helmet={
-          <Helmet
-            titleTemplate="%s | Blog"
-          >
+        helmet={(
+          <Helmet titleTemplate="%s | Blog">
             <title>{`${post.frontmatter.title}`}</title>
             <meta name="description" content={`${post.frontmatter.description}`} />
           </Helmet>
-        }
+)}
         tags={post.frontmatter.tags}
         title={post.frontmatter.title}
       />
     </Layout>
-  )
-}
+  );
+};
 
 BlogPost.propTypes = {
   data: PropTypes.shape({
     markdownRemark: PropTypes.object,
   }),
-}
+};
 
-export default BlogPost
+export default BlogPost;
 
 export const pageQuery = graphql`
   query BlogPostByID($id: String!) {
@@ -99,4 +95,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
